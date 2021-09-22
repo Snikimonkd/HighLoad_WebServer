c22aa6f38e45a0c6a83788f1b995bfeb1c5d8c23
Snikimonkd <cepminkis132@gmail.com>
2021-09-22 18:43:48 +0300

    Динамическая очередь

--- main.c
+++ main.c
@@ -7,12 +7,14 @@
 
 #define THREAD_NUM 4
 
-typedef struct Task {
+typedef struct Task
+{
     int a, b;
     struct Task *next;
 } Task;
 
-typedef struct TaskQueue {
+typedef struct TaskQueue
+{
     Task *head;
     Task *tail;
 } TaskQueue;
@@ -24,17 +26,23 @@ int taskCount = 0;
 pthread_mutex_t mutexQueue;
 pthread_cond_t condQueue;
 
-void executeTask(Task *task) {
+void executeTask(Task *task)
+{
     int result = task->a + task->b;
     printf("The sum of %d and %d is %d\n", task->a, task->b, result);
+    free(task);
 }
 
-void push(Task *task) {
+void push(Task *task)
+{
     pthread_mutex_lock(&mutexQueue);
-    if (taskCount == 0) {
+    if (taskCount == 0)
+    {
         taskQueue.head = task;
         taskQueue.tail = task;
-    } else {
+    }
+    else
+    {
         taskQueue.tail->next = task;
         taskQueue.tail = task;
     }
@@ -43,52 +51,68 @@ void push(Task *task) {
     pthread_cond_signal(&condQueue);
 }
 
-Task *pop() {
+Task *pop()
+{
     Task *buf = taskQueue.head;
-    if (taskCount == 1) {
+    if (taskCount == 1)
+    {
         taskQueue.head = NULL;
         taskQueue.tail = NULL;
-    } else {
+    }
+    else
+    {
         taskQueue.head = taskQueue.head->next;
     }
+
+    taskCount--;
     return buf;
 }
 
-void *startThread(void *args) {
-    while (1) {
-        Task task;
+void *startThread(void *args)
+{
+    while (1)
+    {
+        Task *task;
 
         pthread_mutex_lock(&mutexQueue);
-        while (taskCount == 0) {
+        while (taskCount == 0)
+        {
             pthread_cond_wait(&condQueue, &mutexQueue);
         }
 
-        task = *pop();
-        taskCount--;
+        task = pop();
 
         pthread_mutex_unlock(&mutexQueue);
-        executeTask(&task);
+        executeTask(task);
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     pthread_t th[THREAD_NUM];
     pthread_mutex_init(&mutexQueue, NULL);
     pthread_cond_init(&condQueue, NULL);
-    for (int i = 0; i < THREAD_NUM; i++) {
-        if (pthread_create(&th[i], NULL, &startThread, NULL) != 0) {
+    for (int i = 0; i < THREAD_NUM; i++)
+    {
+        if (pthread_create(&th[i], NULL, &startThread, NULL) != 0)
+        {
             perror("Failed to create the thread");
         }
     }
 
     srand(time(NULL));
-    for (int i = 0; i < 100; i++) {
-        Task t = {.a = rand() % 100, .b = rand() % 100};
-        push(&t);
+    for (int i = 0; i < 100; i++)
+    {
+        Task *t = malloc(sizeof(Task));
+        t->a = rand() % 100;
+        t->b = rand() % 100;
+        push(t);
     }
 
-    for (int i = 0; i < THREAD_NUM; i++) {
-        if (pthread_join(th[i], NULL) != 0) {
+    for (int i = 0; i < THREAD_NUM; i++)
+    {
+        if (pthread_join(th[i], NULL) != 0)
+        {
             perror("Failed to join the thread");
         }
     }
