c21ec75d49a5f83c4c6836b50525739946cf6cdf
Snikimonkd <cepminkis132@gmail.com>
2021-09-28 01:34:50 +0300

    Добавить нужные хэддеры

--- main.c
+++ main.c
@@ -16,19 +16,33 @@
 #define RD_BUFF_MAX 1024
 #define CLIENT_MAX 10
 
-typedef struct myhttp_header {
+char *types[9][2] = {
+    {"js", "application/javascript"},
+    {"html", "text/html"},
+    {"txt", "text/htm"},
+    {"css", "text/css"},
+    {"png", "image/png"},
+    {"jpg", "image/jpeg"},
+    {"jpeg", "image/jpeg"},
+    {"gif", "image/gif"},
+    {"swf", "application/x-shockwave-flash"}};
+
+typedef struct myhttp_header
+{
     char method[5];
     char filename[200];
     char protocol[10];
     char type[100];
 } myhttp_header;
 
-typedef struct Task {
+typedef struct Task
+{
     int sockfd;
     struct Task *next;
 } Task;
 
-typedef struct TaskQueue {
+typedef struct TaskQueue
+{
     Task *head;
     Task *tail;
 } TaskQueue;
@@ -40,20 +54,30 @@ int taskCount = 0;
 pthread_mutex_t mutexQueue;
 pthread_cond_t condQueue;
 
-void error_handle(char *err) {
+void error_handle(char *err)
+{
     perror(err);
     exit(EXIT_FAILURE);
 }
 
-int parse_http_header(char *buff, struct myhttp_header *header) {
-    char *line, *section, *tmp;
+char *get_content_type(char *file_type)
+{
+    for (size_t i = 0; i < 9; ++i)
+    {
+        if (strcmp(file_type, types[i][0]) == 0)
+        {
+            return types[i][1];
+        }
+    }
+
+    return "*/*";
+}
+
+int parse_http_header(char *buff, struct myhttp_header *header)
+{
+    char *section, *tmp;
     int i;
 
-    // fetch the first line
-    // line = strtok(buff, "\n");
-    // fprintf (stdout, "%s\n", line);
-
-    // parse line
     section = strtok(buff, " ");
     strcpy(header->method, section);
     section = strtok(NULL, " ");
@@ -61,151 +85,151 @@ int parse_http_header(char *buff, struct myhttp_header *header) {
     section = strtok(NULL, " ");
     strcpy(header->protocol, section);
 
-    // find request file type
-    section = strtok(NULL, "\n");
-    while (strstr(section, "Accept:") == NULL) {
-        section = strtok(NULL, "\n");
-    }
-    tmp = strtok(section, " ");
-    tmp = strtok(NULL, ",");
-    strcpy(header->type, tmp);
+    char type[400];
+    strcpy(type, header->filename);
+    tmp = strtok(type, ".");
+    tmp = strtok(NULL, " ");
+    strcpy(header->type, get_content_type(tmp));
+
+    printf("Get request with method: %s for %s\n", header->method, header->filename);
+
     return 0;
 }
 
-void send_header(int sockfd, char *code, char *type) {
+void send_header(int sockfd, char *method, char *code, char *type, int length)
+{
+    char buf[450];
 
-    char buf[100];
+    time_t current_time;
+    char *c_time_string;
 
-    if (strcmp(code, "200") == 0) {
+    current_time = time(NULL);
+    c_time_string = ctime(&current_time);
 
-        strcpy(buf, "HTTP/1.0 200 OK\r\n");
-
-        send(sockfd, buf, strlen(buf), 0);
-
-        sprintf(buf, "Content-Type: %s\r\n", type);
-
-        send(sockfd, buf, strlen(buf), 0);
-
-    } else if (strcmp(code, "400") == 0) {
-
-        strcpy(buf, "HTTP/1.0 404 Bad Request\r\n");
-
-        send(sockfd, buf, strlen(buf), 0);
-
-        sprintf(buf, "Content-Type: %s\r\n", type);
-
-        send(sockfd, buf, strlen(buf), 0);
-
-    } else if (strcmp(code, "403") == 0) {
-
-        strcpy(buf, "HTTP/1.0 403 Forbidden\r\n");
-
-        send(sockfd, buf, strlen(buf), 0);
-
-        sprintf(buf, "Content-Type: %s\r\n", type);
-
-        send(sockfd, buf, strlen(buf), 0);
-
-    } else if (strcmp(code, "404") == 0) {
-
-        strcpy(buf, "HTTP/1.0 404 Not Found\r\n");
-
-        send(sockfd, buf, strlen(buf), 0);
-
-        sprintf(buf, "Content-Type: %s\r\n", type);
-
+    if (strcmp(method, "HEAD") == 0)
+    {
+        sprintf(buf,
+                "HTTP/1.0 200 OK\r\n"
+                "Content-Type: %s\r\n"
+                "Content-Length: %d\r\n"
+                "Server: awesome_http_server\r\n"
+                "Connection: close\r\n"
+                "Date: %s\r\n\r\n",
+                type, length, c_time_string);
         send(sockfd, buf, strlen(buf), 0);
     }
 
-    sprintf(buf, "\r\n");
-
-    send(sockfd, buf, strlen(buf), 0);
+    if (strcmp(code, "200") == 0)
+    {
+        sprintf(buf,
+                "HTTP/1.0 200 OK\r\n"
+                "Content-Type: %s\r\n"
+                "Server: awesome_http_server\r\n"
+                "Content-Length: %d\r\n"
+                "Connection: close\r\n"
+                "Date: %s\r\n\r\nkekasdfasdfasdf",
+                type, length, c_time_string);
+        send(sockfd, buf, strlen(buf), 0);
+    }
+    else if (strcmp(code, "400") == 0)
+    {
+        strcpy(buf, "HTTP/1.0 404 Bad Request\r\n");
+        send(sockfd, buf, strlen(buf), 0);
+        sprintf(buf, "Server: awesome_http_server\r\n");
+        send(sockfd, buf, strlen(buf), 0);
+    }
+    else if (strcmp(code, "403") == 0)
+    {
+        strcpy(buf, "HTTP/1.0 403 Forbidden\r\n");
+        send(sockfd, buf, strlen(buf), 0);
+        sprintf(buf, "Server: awesome_http_server\r\n");
+        send(sockfd, buf, strlen(buf), 0);
+    }
+    else if (strcmp(code, "404") == 0)
+    {
+        strcpy(buf, "HTTP/1.0 404 Not Found\r\n");
+        send(sockfd, buf, strlen(buf), 0);
+        sprintf(buf, "Server: awesome_http_server\r\n");
+        send(sockfd, buf, strlen(buf), 0);
+    }
 }
 
-void send_response(int sockfd, struct myhttp_header *header) {
-
+void send_response(int sockfd, struct myhttp_header *header)
+{
     FILE *file;
-
     char filename[200];
-
     char filedata[8092];
-
     char code[4];
-
     int len;
-
-    int nbytes, i;
-
+    int nbytes = 0;
     char *tmp;
-
     char new[200];
 
-    if (strcmp(header->filename, "/") == 0) {
-
+    if (strcmp(header->filename, "/") == 0)
+    {
         strcpy(filename, "index.html");
-
-    } else {
-
+    }
+    else
+    {
         strcpy(filename, ".");
-
         strcat(filename, header->filename);
     }
 
-    // replace the %20
-
-    if (strstr(filename, "%20") != NULL) {
-
+    if (strstr(filename, "%20") != NULL)
+    {
         tmp = strtok(filename, "%20");
-
         strcpy(new, tmp);
-
         strcat(new, " ");
 
-        while ((tmp = strtok(NULL, "%20")) != NULL) {
-
+        while ((tmp = strtok(NULL, "%20")) != NULL)
+        {
             strcat(new, tmp);
-
             strcat(new, " ");
         }
 
         new[strlen(new) - 1] = 0;
-
         strcpy(filename, new);
     }
 
     file = fopen(filename, "r");
 
-    if (file == NULL) {
-
-        if (errno == ENOENT) {
-
-            // not found
-
+    if (file == NULL)
+    {
+        if (errno == ENOENT)
+        {
             strcpy(code, "404");
-
-        } else if (errno == EACCES) {
-
-            // permission denied
-
-            strcpy(code, "402");
-
-        } else {
-
-            // undefined
-
+        }
+        else if (errno == EACCES)
+        {
+            strcpy(code, "403");
+        }
+        else
+        {
             strcpy(code, "404");
         }
 
-        send_header(sockfd, code, header->type);
+        send_header(sockfd, header->method, code, header->type, 0);
+    }
+    else
+    {
+        fseek(file, 0L, SEEK_END);
+        int size = ftell(file) + 2;
 
-    } else {
+        fseek(file, 0L, SEEK_SET);
+
+        send_header(sockfd, header->method, code, header->type, size);
+
+        if (strcmp(header->method, "HEAD") == 0)
+        {
+            puts("HEAD");
+            fclose(file);
+            return;
+        }
 
         strcpy(code, "200");
-
-        send_header(sockfd, code, header->type);
-
-        while ((nbytes = fread(filedata, sizeof(char), 8092, file)) > 0) {
-
+        send_header(sockfd, header->method, code, header->type, size);
+        while ((nbytes = fread(filedata, sizeof(char), 8092, file)) > 0)
+        {
             write(sockfd, filedata, nbytes);
         }
 
@@ -213,34 +237,41 @@ void send_response(int sockfd, struct myhttp_header *header) {
     }
 }
 
-int read_from_client(int sockfd) {
+int read_from_client(int sockfd)
+{
     char buffer[RD_BUFF_MAX];
     int nbytes;
     struct myhttp_header header;
 
     nbytes = read(sockfd, buffer, RD_BUFF_MAX);
-    if (nbytes < 0) {
+    if (nbytes < 0)
+    {
         error_handle("socket read failed");
         return -1;
-    } else if (nbytes == 0) {
+    }
+    else if (nbytes == 0)
+    {
         return -1;
-    } else {
-        // parse_http_header
+    }
+    else
+    {
         parse_http_header(buffer, &header);
-
         send_response(sockfd, &header);
         close(sockfd);
         return 0;
     }
 }
 
-void push(Task *task) {
-    puts("push");
+void push(Task *task)
+{
     pthread_mutex_lock(&mutexQueue);
-    if (taskCount == 0) {
+    if (taskCount == 0)
+    {
         taskQueue.head = task;
         taskQueue.tail = task;
-    } else {
+    }
+    else
+    {
         taskQueue.tail->next = task;
         taskQueue.tail = task;
     }
@@ -249,13 +280,16 @@ void push(Task *task) {
     pthread_cond_signal(&condQueue);
 }
 
-Task *pop() {
-    puts("poop");
+Task *pop()
+{
     Task *buf = taskQueue.head;
-    if (taskCount == 1) {
+    if (taskCount == 1)
+    {
         taskQueue.head = NULL;
         taskQueue.tail = NULL;
-    } else {
+    }
+    else
+    {
         taskQueue.head = taskQueue.head->next;
     }
 
@@ -263,12 +297,15 @@ Task *pop() {
     return buf;
 }
 
-void *startThread(void *args) {
-    while (1) {
+void *startThread(void *args)
+{
+    while (1)
+    {
         Task *task;
 
         pthread_mutex_lock(&mutexQueue);
-        while (taskCount == 0) {
+        while (taskCount == 0)
+        {
             pthread_cond_wait(&condQueue, &mutexQueue);
         }
 
@@ -280,88 +317,68 @@ void *startThread(void *args) {
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int myhttpd_sockfd, client_sockfd;
-    int myhttpd_version;
     int myhttpd_port;
-    int myhttpd_timeout;
-
-    int i;
 
     struct sockaddr_in myhttpd_sockaddr, client_sockaddr;
     socklen_t size;
-
     pthread_t newthread;
 
-    // verify arg first
-    if (argc != 4) {
+    if (argc != 2)
+    {
         error_handle("incorrect number of args");
     }
-    if (strcmp(argv[1], "1") == 0) {
-        // http 1.0
-        myhttpd_version = 0;
-        if (atoi(argv[3]) != 0) {
-            error_handle("timeout for http 1.0 should be zero");
-        }
-    } else if (strcmp(argv[1], "1.1") == 0) {
-        // http 1.1
-        myhttpd_version = 1;
-        if (atoi(argv[3]) <= 0) {
-            error_handle("timeout for http 1.1 should be greater than zero");
-        }
-    } else {
-        perror("incorrect http version");
-        exit(1);
-    }
 
-    // port
-    myhttpd_port = atoi(argv[2]);
-    if (myhttpd_port <= 1024 || myhttpd_port >= 65536) {
+    myhttpd_port = atoi(argv[1]);
+    if (myhttpd_port <= 1024 || myhttpd_port >= 65536)
+    {
         error_handle(
             "port number has to between 1024 and 65536 (not included)");
     }
 
-    // timeout
-    myhttpd_timeout = atoi(argv[3]);
-
-    // init server socket (ipv4, tcp)
     myhttpd_sockfd = socket(PF_INET, SOCK_STREAM, 0);
-    if (myhttpd_sockfd < 0) {
+    if (myhttpd_sockfd < 0)
+    {
         error_handle("myhttpd socket failed to create");
     }
+
     memset(&myhttpd_sockaddr, 0, sizeof(myhttpd_sockaddr));
     myhttpd_sockaddr.sin_family = AF_INET;
     myhttpd_sockaddr.sin_port = htons(myhttpd_port);
     myhttpd_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if (bind(myhttpd_sockfd, (struct sockaddr *)&myhttpd_sockaddr,
-             sizeof(myhttpd_sockaddr)) == -1) {
+             sizeof(myhttpd_sockaddr)) == -1)
+    {
         close(myhttpd_sockfd);
         error_handle("bind failed");
     }
 
-    if (listen(myhttpd_sockfd, CLIENT_MAX) == -1) {
+    if (listen(myhttpd_sockfd, CLIENT_MAX) == -1)
+    {
         close(myhttpd_sockfd);
         error_handle("listen failed");
     }
 
-    //----------------------------------
-
     pthread_t th[THREAD_NUM];
     pthread_mutex_init(&mutexQueue, NULL);
     pthread_cond_init(&condQueue, NULL);
-    for (int i = 0; i < THREAD_NUM; i++) {
-        if (pthread_create(&th[i], NULL, &startThread, NULL) != 0) {
+    for (size_t i = 0; i < THREAD_NUM; i++)
+    {
+        if (pthread_create(&th[i], NULL, &startThread, NULL) != 0)
+        {
             perror("Failed to create the thread");
         }
     }
 
-    while (1) {
+    while (1)
+    {
         size = sizeof(client_sockaddr);
-        client_sockfd =
-            accept(myhttpd_sockfd, (struct sockaddr *)&client_sockaddr, &size);
-
-        if (client_sockfd == -1) {
+        client_sockfd = accept(myhttpd_sockfd, (struct sockaddr *)&client_sockaddr, &size);
+        if (client_sockfd == -1)
+        {
             error_handle("accept error");
         }
 
@@ -370,8 +387,10 @@ int main(int argc, char *argv[]) {
         push(t);
     }
 
-    for (int i = 0; i < THREAD_NUM; i++) {
-        if (pthread_join(th[i], NULL) != 0) {
+    for (size_t i = 0; i < THREAD_NUM; i++)
+    {
+        if (pthread_join(th[i], NULL) != 0)
+        {
             perror("Failed to join the thread");
         }
     }
